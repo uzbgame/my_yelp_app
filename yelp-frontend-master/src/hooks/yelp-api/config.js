6240916c9cf8b278450325bf8c168e60e11dfd98
uzbgame <55582804+uzbgame@users.noreply.github.com>
2023-08-31 00:08:42 +0500

    config.js

--- yelp-frontend-master/src/hooks/yelp-api/config.js
+++ yelp-frontend-master/src/hooks/yelp-api/config.js
@@ -5,4 +5,4 @@ export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yel
 // The Baerer token must be used on a server whose inner workings are not exposed to the outside world
 // For simplicity and the sake of this tutorial, I'll just leave it here like this
 // But just bear in mind: NEVER EVER save a bearer token inside the frontend code
-export const BEARER_TOKEN = '';
+export const BEARER_TOKEN = 'aUaYSqjUOWpTL0z6_hGkaPH0jaQBH8LqEfbNp9KMe5cc_iTNqlXwbrMGZ-H9MzTMgiOV9UgYDx_zSOEF79bwf2nDP_KuqHYvW3GUR0KJaA5A0fdt3rpZS9DH0qCXXYx';
